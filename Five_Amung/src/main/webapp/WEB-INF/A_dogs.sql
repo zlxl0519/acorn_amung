4eb012b2d55fb88d0ead1b67282220f34720da0c
seunghui <zlxl0519@naver.com>
2020-09-18 17:29:19 +0900

    A_dogs테이블, A_reserve테이블 sql 파일 추가

---
+++ Five_Amung/src/main/webapp/WEB-INF/A_dogs.sql
@@ -0,0 +1,11 @@
+create table A_dogs(
+	num number primary key,
+	member_id varchar2(35),--주인 아이디
+	dname varchar2(40),
+	dage number,
+	breed vachar2(50),
+	weight varchar2(10),
+	neutral varchar2(5),
+	gender varchar2(7),
+	memo CLOB
+);
